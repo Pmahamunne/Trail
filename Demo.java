fb717e3585bf0a138cf4d215faf5090cd1e1a280
Prathamesh <p.mahamune1998@gmail.com>
2023-10-18 21:22:36 +0530

    Third commit

--- Demo.java
+++ Demo.java
@@ -4,10 +4,17 @@ public class Demo{
     }
 
 }
+class Multiply extends Demo{
+    public int multi(int a, int b){
+        return a*b;
+    }
 
-class Sum extends Demo {
+}
+
+class Sum extends Multiply {
     public static void main(String[] args) {
         Sum sum = new Sum();
+        System.out.println(sum.multi(23, 67));
         System.out.println(sum.sum(1, 45));
         
     }
