0029b9834276b73d45331ba5692d21b35d31ad97
Prathamesh <p.mahamune1998@gmail.com>
2023-10-18 21:50:48 +0530

    First commit

--- Demo.java
+++ Demo.java
@@ -11,9 +11,24 @@ class Multiply extends Demo{
 
 }
 
-class Sum extends Multiply {
+class Divide extends Multiply{
+    public int divide(int a, int b){
+        return a/b;
+    }
+}
+class Minus extends Divide{
+    public int minus(int a, int b){
+        return a/b;
+    }
+}
+
+
+class Sum extends Minus {
     public static void main(String[] args) {
         Sum sum = new Sum();
+        System.out.println(sum.minus(34, 12));
+
+        System.out.println(sum.divide(34, 12));
         System.out.println(sum.multi(23, 67));
         System.out.println(sum.sum(1, 45));
         
